ba9d8be70589e325fbb836b0e2a3b42384e74b91
Shea Andrews <s.j.f.andrews@gmail.com>
2018-05-16 17:05:00 -0400

    Rules 3-9 created in Snakefile

--- scripts/R/assign_sex_label.R
+++ scripts/R/assign_sex_label.R
@@ -1,7 +1,8 @@
 args = commandArgs(trailingOnly = TRUE) # Set arguments from the command line
 
 samples.file = args[1] # Samples file
+outfile = args[2] # Samples file
 
 x = read.table(samples.file, header = F, sep = '\t')
 x$V2 = 'M'
-write.table(x, samples.file, col.names = F, row.names = F, sep = '\t', quote = F)
+write.table(x, outfile, col.names = F, row.names = F, sep = '\t', quote = F)
