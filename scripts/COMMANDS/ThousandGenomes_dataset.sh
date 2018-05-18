5b96091c02792f865c71f17c26ec5b05d69ef4ff
Shea Andrews <s.j.f.andrews@gmail.com>
2018-05-18 17:20:32 -0400

    thousand genomes vcf cleaning

--- scripts/COMMANDS/ThousandGenomes_dataset.sh
+++ scripts/COMMANDS/ThousandGenomes_dataset.sh
@@ -1,5 +1,17 @@
-bcftools norm -f ~/Dropbox/src/MitoImpute/data/rCRS.fasta \
--m -any -Oz -o ~/Dropbox/src/MitoImpute/data/ThousandGenomes/ALL.chrMT.vcf.gz \
-~/Dropbox/src/MitoImpute/data/ThousandGenomes/ALL.chrMT.phase3_callmom-v0_4.20130502.genotypes.vcf.gz
+## FTP 1000 genomes mitochondrial sequences
+wget http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chrMT.phase3_callmom-v0_4.20130502.genotypes.vcf.gz -P data/ThousandGenomes/
+wget http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chrMT.phase3_callmom-v0_4.20130502.genotypes.vcf.gz.tbi -P data/ThousandGenomes/
 
-bcftools +fill-tags -Oz -o ~/Dropbox/src/MitoImpute/data/ThousandGenomes/ALL.chrMT2.vcf.gz ~/Dropbox/src/MitoImpute/data/ThousandGenomes/ALL.chrMT.vcf.gz
+## Normalize indels, split multialleics, fill info
+bcftools norm -f data/rCRS.fasta -m - \
+data/ThousandGenomes/ALL.chrMT.phase3_callmom-v0_4.20130502.genotypes.vcf.gz | \
+bcftools view -V indels,mnps | \
+bcftools norm -m + | \
+bcftools +fill-tags -Oz -o data/ThousandGenomes/ALL.chrMT.vcf.gz
+
+## Decompose
+vt decompose data/ThousandGenomes/ALL.chrMT.vcf.gz | \
+bcftools +fill-tags -Oz -o data/ThousandGenomes/ALL.chrMT_decomposed.vcf.gz
+
+## Keep Only first alternate
+./scripts/PYTHON/pickFirstAlt data/ThousandGenomes/ALL.chrMT_decomposed.vcf.gz | bgzip > data/ThousandGenomes/ALL.chrMT_decomposed_noMulti.vcf.gz
