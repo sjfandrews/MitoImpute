fb6fd396dfefce15266cf22d7007e8d676905ec5
Tim McInerney <tim.mcinerney@anu.edu.au>
2018-05-28 12:58:51 +1000

    Added script to make a regions file for subsetting VCF based on presence of SNP in mtDNA SNP chips

--- scripts/R/identify_mtStrands.R
+++ scripts/R/identify_mtStrands.R
@@ -10,7 +10,7 @@ if (!endsWith(strand.dir, "/")) {
 }
 
 b37 <- list.files(strand.dir, pattern = "*.strand")
-b37 <- b37[-grep('.zip', b37)]
+#b37 <- b37[-grep('.zip', b37)]
 
 b37.ls <- lapply(b37, function(x){
   cat(x, 'IN PROGRESS \n')
@@ -25,7 +25,39 @@ b37.ls <- lapply(b37, function(x){
   }
 })
 
-platforms <- do.call(bind_rows, test)
+platforms <- do.call(bind_rows, b37.ls)
 platforms %>% arrange(-n) %>% print(n = Inf)
 
 write_tsv(platforms, paste0(out.dir, "MT_platforms.txt"))
+
+strand.files = list.files(strand.dir, pattern = "_MT_snps.txt")
+tmp = read.table(paste0(strand.dir, strand.files[2]), header = T)
+m = matrix(0, nrow = length(strand.files), ncol = 16569)
+
+for (i in 1:length(strand.files)) {
+  tmp = read.table(paste0(strand.dir, strand.files[i]), header = T)
+  td = c()
+  for (j in sort(tmp$pos)) {
+    m[i,j] = 1
+    td = c(td, paste0("MT\t", j))
+  }
+  file.out = file(paste0(out.dir, "chip_regions/", strand.files[i]))
+  writeLines(td, file.out)
+  close(file.out)
+}
+
+row.names(m) = strand.files
+mtn = c(1:16569)
+mtn = unlist(lapply(mtn, function(x) paste0("MT.", x)))
+colnames(m) = mtn
+
+M = m[, colSums(m != 0) > 0]
+write.csv(M, paste0(out.dir, "SNP_inclusion_matrix.csv"), quote = F)
+
+for (i in 1:nrow(M)) {
+  print(row.names(M)[i])
+}
+
+png(paste0(out.dir, "heat_map.png"), width = 30, height = 20, units = "in", res = 300)
+heatmap(M, Rowv=NA, Colv=NA, scale="column")
+dev.off()
