0191e91735bbe11cf4c87f7436f63e5f937bd6b0
Tim McInerney <tim.mcinerney@anu.edu.au>
2018-07-22 15:47:24 +1000

    edited ref panel snake file v2

---
+++ scripts/BASH/PlatformStrandFiles_Raijin.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#PBS -P te53
+#PBS -q normal
+#PBS -l walltime=12:00:00
+#PBS -l mem=8GB
+#PBS -l ncpus=1
+#PBS -N PlatformStrandFiles
+#PBS -m be
+#PBS -M u5015730@anu.edu.au
+#PBS -j oe
+#PBS -o /g/data1a/te53/MitoImpute/logs/
+
+# LOAD THE MODULE
+module unload intel-fc intel-cc
+module load intel-fc/16.0.3.210
+module load intel-cc/16.0.3.210
+module load Rpackages/3.4.3
+module load R/3.4.3
+module load python3/3.6.2
+pyvenv /g/data1a/te53/MitoImpute/Py3virt
+source /g/data1a/te53/MitoImpute/Py3virt/bin/activate
+module load vt
+module load bcftools/1.8
+module avail impute2
+
+# RUN THE COMMANDS
+cd ~/GitCode/MitoImpute
+snakemake -s PlatformStrandFiles.smk
