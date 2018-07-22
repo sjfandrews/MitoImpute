0a75bf559a15b46cef7f7f64b3c3987e326cbfda
Tim McInerney <tim.mcinerney@anu.edu.au>
2018-07-22 12:40:53 +1000

    edited ref panel snake file v2

--- scripts/BASH/Reference_panel_v2_Raijin.sh
+++ scripts/BASH/Reference_panel_v2_Raijin.sh
@@ -8,13 +8,20 @@
 #PBS -m be
 #PBS -M u5015730@anu.edu.au
 #PBS -j oe
-#PBS -o /g/data1a/te53/haploco/logs/
+#PBS -o /g/data1a/te53/MitoImpute/logs/
 
 # LOAD THE MODULE
-initPy3virt_MitoImpute
-initR
+module unload intel-fc intel-cc
+module load intel-fc/16.0.3.210
+module load intel-cc/16.0.3.210
+module load Rpackages/3.4.3
+module load R/3.4.3
+module load python3/3.6.2
+pyvenv /g/data1a/te53/MitoImpute/Py3virt
+source /g/data1a/te53/MitoImpute/Py3virt/bin/activate
 module load vt
 module load bcftools/1.8
+module avail impute2
 
 # RUN THE COMMANDS
 cd ~/GitCode/MitoImpute
