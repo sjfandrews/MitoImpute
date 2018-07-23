97f97ef20a5b95d9cb00228c87affa56f4b46265
Tim McInerney <tim.mcinerney@anu.edu.au>
2018-07-23 10:28:01 +1000

    added IMPUTE2 script for RAIJIN

--- scripts/BASH/IMPUTE2_ADNI.sh
+++ scripts/BASH/IMPUTE2_ADNI.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -P te53
-#PBS -q express
-#PBS -l walltime=00:01:00
-#PBS -l mem=1GB
+#PBS -q normal
+#PBS -l walltime=48:00:00
+#PBS -l mem=32GB
 #PBS -l ncpus=1
 #PBS -N IMPUTE2_ADNI
 #PBS -m be
@@ -17,7 +17,7 @@ module load impute2/2.3.2
 REF_DIR="/g/data1a/te53/MitoImpute/data/REF_PANEL/"
 REF_FILE="${REF_DIR}Reference_panel_v2"
 ADNI_FILE="${REF_DIR}ADNI_samples"
-echo ${REF_FILE}
-echo ${ADNI_FILE}
 
+echo "COMMAND RUN:"
 echo impute2 -chrX -m ${REF_FILE}.map -h ${REF_FILE}.hap.gz -l ${REF_FILE}.legend.gz -g ${ADNI_FILE}.gen.gz -sample_g ${ADNI_FILE}.samples -int 1 16569 -Ne 20000 -o ${ADNI_FILE}_IMPUTED
+impute2 -chrX -m ${REF_FILE}.map -h ${REF_FILE}.hap.gz -l ${REF_FILE}.legend.gz -g ${ADNI_FILE}.gen.gz -sample_g ${ADNI_FILE}.samples -int 1 16569 -Ne 20000 -o ${ADNI_FILE}_IMPUTED
