bb70073f7070363c95888d8d403f3ce9247de8ac
Tim McInerney <tim.mcinerney@anu.edu.au>
2018-07-23 10:24:37 +1000

    added IMPUTE2 script for RAIJIN

--- scripts/BASH/IMPUTE2_ADNI.sh
+++ scripts/BASH/IMPUTE2_ADNI.sh
@@ -16,8 +16,8 @@ module load impute2/2.3.2
 # RUN THE COMMANDS
 REF_DIR="/g/data1a/te53/MitoImpute/data/REF_PANEL/"
 REF_FILE="${REF_DIR}Reference_panel_v2"
+ADNI_FILE="${REF_DIR}ADNI_samples"
 echo ${REF_FILE}
+echo ${ADNI_FILE}
 
-#impute2 \
-#-chrX \
-#-m McInerney_Master_Alignment_Nov30_2017_ambig2missing_highQual_filtered map -h McInerney_Master_Alignment_Nov30_2017_ambig2missing_highQual_filtered hap.gz -l McInerney_Master_Alignment_Nov30_2017_ambig2missing_highQual_filtered legend.gz -g ADNI_samples gen.gz -sample_g ADNI_samples samples -int 1 16569 -Ne 20000 -o ADNI_samples_IMPUTED
+echo impute2 -chrX -m ${REF_FILE}.map -h ${REF_FILE}.hap.gz -l ${REF_FILE}.legend.gz -g ${ADNI_FILE}.gen.gz -sample_g ${ADNI_FILE}.samples -int 1 16569 -Ne 20000 -o ${ADNI_FILE}_IMPUTED
