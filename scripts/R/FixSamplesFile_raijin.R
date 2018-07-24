462406fb1975e24f98ef3e59925dd695ea18fae3
Tim McInerney <tim.mcinerney@anu.edu.au>
2018-07-25 09:26:20 +1000

    added ref panel 2 files

--- scripts/R/FixSamplesFile_raijin.R
+++ scripts/R/FixSamplesFile_raijin.R
@@ -2,8 +2,9 @@
 ## Read in Arguments 
 # 1: Current working directory 
 args = commandArgs(trailingOnly=TRUE)
-file.path <- args[1]
+file.in <- args[1]
 
-dat <- read.table(file.path, sep = " ", header = T)
+dat <- read.table(file.in, sep = " ", header = T)
 dat[1,4] <- "D"
-write.table(dat, file.path, sep = " ", col.names = T, row.names = F, quote = F)
+write.table(dat, file.in, sep = " ", col.names = T, row.names = F, quote = F)
+message(paste0(file.in, " SUCCESSFULLY CORRECTED"))
